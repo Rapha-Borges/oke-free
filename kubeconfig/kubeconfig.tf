ec4ac1b1a36aed578ba989730b4c0c7fbe6507cc
Raphael Borges <raps_rnb@hotmail.com>
2024-01-20 14:02:31 -0300

    Update kubeconfig.tf

--- kubeconfig/kubeconfig.tf
+++ kubeconfig/kubeconfig.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "create_kubeconfig" {
   provisioner "local-exec" {
-    command = "oci ce cluster create-kubeconfig --cluster-id ${var.cluster_id} --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --profile DEFAULT"
+    command = "oci ce cluster create-kubeconfig --cluster-id ${var.cluster_id} --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT"
   }
 }
