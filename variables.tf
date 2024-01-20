ae14f1b39208f30a2626bf920db5c6dcdb9a4d97
Raphael Borges <raps_rnb@hotmail.com>
2024-01-20 13:50:56 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -90,18 +90,18 @@ variable "ssh_private_key" {
   default = "id_rsa"
 }
 
-variable "fingerprint" {
-  type    = string
-}
+# variable "fingerprint" {
+#   type    = string
+# }
 
-variable "private_key_path" {
-  type    = string
-}
+# variable "private_key_path" {
+#   type    = string
+# }
 
-variable "tenancy_ocid" {
-  type    = string
-}
+# variable "tenancy_ocid" {
+#   type    = string
+# }
 
-variable "user_ocid" {
-  type    = string
-}
+# variable "user_ocid" {
+#   type    = string
+# }
